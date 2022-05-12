f2b706f98ce946def038672b6eff461fded4cf38
Kevin Borup - Mobile <kevi1291@zbc.dk>
2022-05-12 09:08:53 +0200

    HelpJacob

---
+++ ConsoleApp_HelpJacob/MessageSender.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_HelpJacob
+{
+    internal class MessageSender
+    {
+        public void SendMessage(MessageMethod.MessageCarrier type, Message m, bool isHTML)
+        {
+            //herinde sendes der en email ud til modtageren
+            if (type.Equals(MessageMethod.MessageCarrier.Smtp))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via Smtp
+            }
+
+            if (type.Equals(MessageMethod.MessageCarrier.VMessage))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via VMessage
+            }
+        }
+        
+        public void SendMessageToAll(MessageMethod.MessageCarrier type, string[] to, Message m, bool isHTML)
+        {
+            if (type.Equals(MessageMethod.MessageCarrier.Smtp))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via Smtp
+            }
+
+            if (type.Equals(MessageMethod.MessageCarrier.VMessage))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via VMessage
+            }
+        }
+
+        private string ConvertBodyToHTML(string plainText)
+        {
+            return "" + plainText + "";
+        }
+    }
+}
---
+++ ConsoleApp_HelpJacob/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_HelpJacob;
+
+Console.WriteLine("Hello, World!");
+
+
+namespace thisNamespace
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            MessageSender sender = new MessageSender();
+
+            string to = "";
+            string[] toAll = new string[2] { "", "" };
+            string from = "";
+            string body = "";
+            string subject = "";
+            string cc = "";
+
+            Message message = new Message(to, from , body, subject, cc);
+
+            sender.SendMessage(MessageMethod.MessageCarrier.VMessage, message, true);
+            sender.SendMessageToAll(MessageMethod.MessageCarrier.VMessage, toAll, message, true);
+        }
+    }
+
+}
